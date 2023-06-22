9eb0a53deb84480df65c06d4b92363126de7336a
thenikitakotlyarov <thenikitakotlyarov@gmail.com>
2023-06-21 23:34:53 -0600

    added a little more to the install script; not sure if it will work, tbh, but worth having something, right?

--- install.sh
+++ install.sh
@@ -1,30 +1,56 @@
 #!/bin/bash
 
-if [ -f /etc/os-release ]; then
-    # shell will source this file and set the $ID variable
-    . /etc/os-release
+# Determine the operating system
+OS=$(uname -s)
+
+# Check the operating system and install dependencies accordingly
+if [[ "$OS" == "Linux" ]]; then
+    # Linux dependencies
+    if command -v apt-get &>/dev/null; then
+        # APT package manager (Debian, Ubuntu, etc.)
+        echo "Installing Linux dependencies using APT..."
+        sudo apt-get update
+        sudo apt-get install -y libncursesw6 libnoise1 libssl3 libcrypto3 libsdl2-2.0-0 libsdl2-mixer-2.0-0 libstdc++6 libgcc1
+        echo "Linux dependencies installed."
+    elif command -v yum &>/dev/null; then
+        # YUM package manager (CentOS, Fedora, RHEL, etc.)
+        echo "Installing Linux dependencies using YUM..."
+        sudo yum update
+        sudo yum install -y ncurses libnoise openssl openssl-devel SDL2 SDL2_mixer libstdc++ libgcc
+        echo "Linux dependencies installed."
+    elif command -v dnf &>/dev/null; then
+        # DNF package manager (Fedora, RHEL 8+, etc.)
+        echo "Installing Linux dependencies using DNF..."
+        sudo dnf update
+        sudo dnf install -y ncurses libnoise openssl openssl-devel SDL2 SDL2_mixer libstdc++ libgcc
+        echo "Linux dependencies installed."
+    else
+        echo "Unsupported Linux package manager."
+        exit 1
+    fi
+elif [[ "$OS" == "Darwin" ]]; then
+    # macOS dependencies (Homebrew required)
+    echo "Installing macOS dependencies..."
+    brew update
+    brew install ncurses libnoise sdl2 sdl2_mixer
+    echo "macOS dependencies installed."
+elif [[ "$OS" == "Windows" ]]; then
+    # Windows dependencies
+    echo "Please install the following dependencies on Windows manually:"
+    echo "- libncursesw"
+    echo "- libnoise"
+    echo "- libssl"
+    echo "- libcrypto"
+    echo "- SDL2"
+    echo "- SDL2_mixer"
+    echo "- libstdc++"
+    echo "- libgcc"
+else
+    echo "Unsupported operating system: $OS"
+    exit 1
 fi
 
-install_deps() {
-    echo "Installing dependencies..."
-    $@ -y install ncurses libnoise-dev libssl-dev libcrypto++-dev libsdl2-dev libsdl2-mixer-dev libstdc++6
-    echo "Done!"
-}
-
-case $ID in
-    "ubuntu"|"debian")
-        install_deps apt-get update && apt-get upgrade
-        ;;
-    "centos"|"rhel"|"fedora")
-        install_deps yum update && yum upgrade
-        ;;
-    "suse"|"opensuse")
-        install_deps zypper refresh && zypper up
-        ;;
-    *)
-        echo "Sorry, your distribution is not supported by this script."
-        exit 1
-        ;;
-esac
-
+# Run your game
+echo "Running the game..."
+./file_crawler
 
