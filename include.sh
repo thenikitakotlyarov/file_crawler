ffa648929181bb3ea548015c63b3e8dea9528b4a
thenikitakotlyarov <thenikitakotlyarov@gmail.com>
2023-07-20 10:49:11 -0600

    cleaned up repo

--- include.sh
+++ include.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-cd src
+SRC_DIR="src"
+
+cd $SRC_DIR
+
+
 STRUCT_FILE="struct.txt"
 INCLUDE_FILE="include.h"
 
+
 find | grep '\.h' > $STRUCT_FILE
+
 
 echo '' > $INCLUDE_FILE
 
@@ -13,6 +19,8 @@ for l in $(cat $STRUCT_FILE); do
     echo "#include \"$(echo $l | cut -c 3- )\"" >> $INCLUDE_FILE
     fi
 done
+
+rm $STRUCT_FILE
 
 cd ../
 less src/$INCLUDE_FILE
--- run_in_kitty.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-kitty -e ./build/file_crawler 2>/dev/null
-
